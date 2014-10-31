11e0c15bd2cef8fde393354a5fe2abb12016e766
agconti <agc11d@gmail.com>
2014-10-31 09:39:01 -0400

    feat(lesson/4): complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-process.stdin.pipe(process.stdout)
+var through = require('through')
+
+var tr = through(function write (buff) {
+    // uppercase incoming words 
+    this.queue(buff.toString().toUpperCase())
+})
+
+process.stdin.pipe(tr).pipe(process.stdout)
